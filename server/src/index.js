93eb32520bda407c3ec15251d26d5878354cd565
juma <raisben7@gmail.com>
2025-07-03 17:21:04 -0700

    serve static files from client build

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 import "@dotenvx/dotenvx/config";
 import express from "express";
 import morgan from "morgan";
+import path from "path";
 import healthRoutes from "./routes/health.routes.js";
 
+const __dirname = path.resolve();
 const app = express();
-app.set("trust proxy", true);
 
 // Middleware
 app.use(express.json());
@@ -13,8 +14,15 @@ process.env.NODE_ENV === "production"
   : app.use(morgan("dev"));
 
 // API routes
-app.use("/api", healthRoutes);
+app.use("/api/sys", healthRoutes);
 
+// Static files from client build (production)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
