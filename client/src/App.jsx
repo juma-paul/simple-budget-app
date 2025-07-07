1a603ff741778b86ec058929b8ab1af2d2951068
juma <raisben7@gmail.com>
2025-07-07 13:35:42 -0700

    chore: configure routes and include header on all routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Dashboard from "./pages/Dashboard";
+import LogIn from "./pages/LogIn";
+import Reviews from "./pages/Reviews";
+import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      {/* Header */}
       <Header />
-    </div>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
