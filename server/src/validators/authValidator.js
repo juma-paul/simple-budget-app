83836917dd6196e1acf8756a418ccc999859b191
juma <raisben7@gmail.com>
2025-07-08 19:33:57 -0700

    feat: add auth validator

---
+++ server/src/validators/authValidator.js
@@ -0,0 +1,23 @@
+import { body } from "express-validator";
+
+export const registerValidation = [
+  body("email")
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .normalizeEmail(),
+
+  body("username")
+    .trim()
+    .isLength({ min: 4, max: 12 })
+    .withMessage("Username must be between 3-12 characters")
+    .isAlphanumeric()
+    .withMessage("Username must contain only letters and numbers."),
+
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be atleast 8 characters.")
+    .matches(/[a-zA-Z]/)
+    .withMessage("Password must include at least one letter.")
+    .matches(/[\d\W_]/)
+    .withMessage("Password must include at least one number or symbol."),
+];
