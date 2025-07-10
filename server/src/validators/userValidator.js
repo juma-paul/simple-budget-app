90eb45eaad88b78c3f2d658cdf859d54c372a678
juma <raisben7@gmail.com>
2025-07-10 09:15:32 -0700

    feat: add validator for user inputs during updates

---
+++ server/src/validators/userValidator.js
@@ -0,0 +1,40 @@
+import { body } from "express-validator";
+
+export const updateUserValidation = [
+  body("email")
+    .optional()
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .normalizeEmail(),
+
+  body("username")
+    .optional()
+    .trim()
+    .isLength({ min: 4, max: 12 })
+    .withMessage("Username must be between 4-12 characters.")
+    .matches(/^[a-zA-Z0-9_-]+$/)
+    .withMessage(
+      "Username may include letters, numbers, underscores, or dashes."
+    ),
+
+  body("firstName").optional().trim(),
+  body("lasttName").optional().trim(),
+
+  body("newPassword")
+    .optional()
+    .isLength({ min: 8 })
+    .withMessage("Password must be atleast 8 characters.")
+    .matches(/[a-zA-Z]/)
+    .withMessage("Password must include at least one letter")
+    .matches(/[\d\W_]/)
+    .withMessage("Password must include atleast one number or symbol."),
+
+  body("confirmPassword")
+    .optional()
+    .custom((value, { req }) => {
+      if (value !== req.body.newPassword) {
+        throw new Error("Passwords do not match.");
+      }
+      return true;
+    }),
+];
