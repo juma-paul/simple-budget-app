c7c9cc58de0c4e689df2392aaf2efe4b349082eb
juma <raisben7@gmail.com>
2025-08-06 16:44:41 -0700

    feat: add budget to redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,14 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice.js";
 import uiReducer from "./features/ui/uiSlice.js";
+import budgetReducer from "./features/budget/budgetSlice.js";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 
 const rootReducer = combineReducers({
   user: userReducer,
   ui: uiReducer,
+  budget: budgetReducer,
 });
 
 const persistConfig = {
