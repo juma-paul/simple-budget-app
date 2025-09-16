b4e57d8bb43aa4c370cc0d3ac84eee617d088570
juma <raisben7@gmail.com>
2025-09-15 23:56:50 -0700

    style: add h-full to pass App.jsx configs to main tag in dashboard

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,5 +1,3 @@
-// Layout for all components
-
 export default function Layout({ children }) {
-  return <div className="pt-12 tablet:pt-14 laptop:pt-16">{children}</div>;
+  return <div className="h-full pt-12 tablet:pt-14 laptop:pt-16">{children}</div>;
 }
