0715b2b784fd3d05d70b96d449063f1ee0a01d5a
juma <raisben7@gmail.com>
2025-09-15 23:54:54 -0700

    refactor: modify Private route to prevent users from navigating to unauthorized routes

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,7 +1,15 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuthStore } from "../store/authStore";
 
 export default function PrivateRoute() {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-}
+  const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
+  const user = useAuthStore((s) => s.user);
+  const location = useLocation();
+
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
+  if (user?.isDeleted && location.pathname !== "/dashboard/profile") {
+    return <Navigate to="/dashboard/profile" replace />;
+  }
+
+  return <Outlet />;
+};
